56d53bc11cedb97b66764bc071107b6fed15419d
AriFlo <ariflo@arianflores.com>
2015-11-04 13:47:59 -0800

    ran through each example and completed all on console

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("sanity check!")
+var firstParagraph = document.getElementById('main');
+console.log(firstParagraph);
+
+var getAllElements = document.getElementsByTagName('ul');
+console.log(getAllElements)
+
+var getSingleElement = document.getElementsByTagName('ul')[0];
+console.log(getSingleElement)
+
+var allListItems = document.getElementsByTagName('li');
+
+
+for (var i = 0; i < allListItems.length; i++) {
+  console.log(allListItems[i]);
+}
+
+var evenElements = document.getElementsByClassName('even');
+console.log(evenElements)
+
+var oddElements = document.getElementsByClassName('odd');
+console.log(oddElements)
+
+var firstEvenElement = document.getElementsByClassName('even')[0];
+console.log(firstEvenElement)
+
+var firstOddElement = document.getElementsByClassName('odd')[0];
+console.log(firstOddElement)
+
+var main = document.querySelector('#main');
+console.log(main);
+
+var oddAndEvenElements = document.querySelectorAll('.odd, .even');
+console.log(oddAndEvenElements);
